272382b0c12c929562e591614900130f32883296
Daniel Ingraham <daniel@danielingraham.com>
2016-11-21 21:25:56 -0500

    Got rid of a swear curse

--- Assets/TimeManager.cs
+++ Assets/TimeManager.cs
@@ -9,11 +9,10 @@ namespace Managers {
 	{
 		public FloatReactiveProperty frameToMinuteConversion { get; set; }
 
-		//TODO most of the below is fucked and out of date. This should just be pushing ticks to clock and other observers that care.
 		public TimeManager (float conversionFactor)
 		{
 			Debug.Log ("Loaded TimeManager");
-
+			//TODO I don't think we need this anymore. Evaluate!
 			this.frameToMinuteConversion = new FloatReactiveProperty(conversionFactor);
 		}
 
